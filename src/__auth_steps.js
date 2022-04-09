c932cb17b20b5cee21db3e93553725f0f232cead
Meherab211 <meherab395@gmail.com>
2022-04-09 23:17:47 +0600

    initical firebase setup

---
+++ src/__auth_steps.js
@@ -0,0 +1,15 @@
+/* 
+1.create a new projects in consolelog.google
+2.npm install firebase
+3.create firebase.init.js and import getAuth from firebase/auth
+4.go to Authentication and allow to email
+5.
+
+*/
+
+/* 
+1.hosting steps :npm install -g firebase-tools
+2.firebase login
+3.  firebase init
+4.firebase deploy
+*/
